7978d115e632d8f62ac07ac6cb47f9588b6cbff1
suntong <suntong@cpan.org>
2022-12-04 18:09:55 +0000

    - [+] add /upload handler

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express')
+const multer = require('multer')
+
 const app = express()
-const port = 3399
+const port = process.env.PORT || 3399
+
+const uploader = multer({
+    storage: multer.diskStorage({}),
+    limits: {
+        fileSize: 50 * 1024 * 1024,
+        fieldSize: 50 * 1024 * 1024,
+      }
+    })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post("/upload", uploader.single("file"), async (req, res) => {
+    // never get here as multer rejects as 413 (too large)
+    res.json({
+        success: true,
+        file: req.file,
+    })
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
